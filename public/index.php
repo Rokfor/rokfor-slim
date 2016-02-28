1c69302328c7048a17b0ec587f93a8935ab3408d
urshofer <u.hofer@tr51.org>
2016-02-28 21:38:13 +0100

    updated settings links

--- public/index.php
+++ public/index.php
@@ -1,24 +1,27 @@
 <?php
+
+/* To help the built-in PHP dev server, check if the request was actually for
+ * something which should probably be served as a static file
+ */
 if (PHP_SAPI == 'cli-server') {
-    // To help the built-in PHP dev server, check if the request was actually for
-    // something which should probably be served as a static file
     $file = reset(explode("?",__DIR__ . $_SERVER['REQUEST_URI']));
     if (is_file($file)) {
         return false;
     }
 }
 
+/* Only used in development, clearing compiled templates on every call
 foreach (glob(__DIR__ . '/../cache/*') as $file) {
   unlink($file);
 }
-
+*/
 
 require __DIR__ . '/../vendor/autoload.php';
 
 session_start();
 
 // Instantiate the app
-$settings = require __DIR__ . '/../src/settings.php';
+$settings = require __DIR__ . '/../config/settings.php';
 
 date_default_timezone_set($settings['settings']['timezone']);
 
@@ -38,6 +41,9 @@ require __DIR__ . '/../src/middleware.php';
 
 // Register routes
 require __DIR__ . '/../src/routes.php';
+
+// Register API specific routes
+require __DIR__ . '/../src/api.php';
 
 // Run app
 $app->run();
