f6f378946c804dbd4b38e4b42555f37d6748f10c
urshofer <u.hofer@tr51.org>
2016-04-25 11:01:56 +0200

    moved redis to own config file

---
+++ config/redis.local.php
@@ -0,0 +1,9 @@
+<?php
+
+// Redis Cache settings
+
+return [
+  'redis'     => true,                            // Enable Redis Cache
+  'redis_ip'  => '127.0.0.1',                     // Ip of Redis Cache Server
+  'redis_port'=> 6379,                            // Ip of Redis Cache Server  
+];
