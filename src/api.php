6af0cfcb84f03239d82cd9a6e3b1cf2432b538a0
urshofer <u.hofer@tr51.org>
2016-08-31 14:57:55 +0200

    add debug

--- src/api.php
+++ src/api.php
@@ -524,7 +524,8 @@ $app->get('/asset/{id:[0-9]*}/{field:[0-9]*}/{file:.+}', function ($request, $re
   $f = $this->db->getField($args['field']);
   $_isnginx = (strpos($_SERVER['SERVER_SOFTWARE'], 'nginx') !== false);
 
-  echo $_SERVER['SERVER_SOFTWARE'];
+  print_r($_SERVER['SERVER_SOFTWARE']);
+  exit(0);
   
   
   if ($c && $c->getId() == $args['id'] && $c->getTemplatenames()->getPublic() === "1" && stristr($f->getContent(), $args['file'])) {
