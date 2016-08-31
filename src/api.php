9b1f1f01a7a8b83644e496311bd707c3af1152bd
urshofer <u.hofer@tr51.org>
2016-08-31 16:05:10 +0200

    rem debug

--- src/api.php
+++ src/api.php
@@ -526,8 +526,7 @@ $app->get('/asset/{id:[0-9]*}/{field:[0-9]*}/{file:.+}', function ($request, $re
   
   if ($c && $c->getId() == $args['id'] && $c->getTemplatenames()->getPublic() === "1" && stristr($f->getContent(), $args['file'])) {
     if ($_isnginx === true) {
-      echo('X-Accel-Redirect: /internal_redirect/' . str_replace('https://', '', $this->db->presign_file($args['file'])));
-      exit(0);
+      header('X-Accel-Redirect: /internal_redirect/' . str_replace('https://', '', $this->db->presign_file($args['file'])));
     }
     else {
       header('Location: ' . $this->db->presign_file($args['file']));
