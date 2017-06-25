138d7cffe3a60f642f7e66533ab2f16600448a75
Urs Hofer <urshofer@users.noreply.github.com>
2017-06-25 15:10:01 +0200

    edited comment

--- config/session.local.php
+++ config/session.local.php
@@ -2,6 +2,12 @@
 
 // Session Handler
 
+// If handler is not equal to 'redis', the default session handler
+// will be used.
+// If you are running rokfor in a distributed environment, i.E on Amazon
+// Cloud Services, it is important that sessions are distributed among the
+// various instances.
+
 return [
   'handler'     => 'redis',                            // Enable Redis Cache
   'redis_ip'    => '127.0.0.1',                     // Ip of Redis Cache Server
